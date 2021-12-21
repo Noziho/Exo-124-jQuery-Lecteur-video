8fe9d42e92dcca330006fef2fa11cc01dd69089f
Noah2211 <noah.decroix3@gmail.com>
2021-12-21 14:55:37 +0100

    exercice done except last part

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 // FIXME Assurez vous que ces deux variables contiennent bien les bonnes choses ;-)
-let player = $('.player__video').first();
-let progress = $('.progress__filled').first();
+let player = $('.player__video').get(0);
+
+let progress = $('.progress__filled');
+
+let buttonPlay = $('.toggle');
+
+let counter = 3
+
+buttonPlay.click(function () {
+
+    counter --;
+    if (counter === 2) {
+        player.play();
+        buttonPlay.text('||');
+
+    }
+
+    if (counter === 1) {
+        player.pause();
+        buttonPlay.text('►')
+        counter = 3;
+    }
+
+})
